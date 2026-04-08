6bc8b72dd9fa4079d984ef05551ff4da9cc7c659
basel118200 <164511769+basel118200@users.noreply.github.com>
2026-04-08 19:27:31 +0200

    Your message

--- next.config.js
+++ next.config.js
@@ -2,6 +2,14 @@
 const nextConfig = {
   distDir: process.env.NODE_ENV === 'development' ? '.next-dev' : '.next',
   output: 'standalone',
+  async rewrites() {
+    return [
+      {
+        source: '/api/:path*',
+        destination: 'https://biobeats.duckdns.org/api/:path*',
+      },
+    ];
+  },
 };
 
 module.exports = nextConfig;
